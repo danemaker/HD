4882cec40a1b113f9a4e8a6166abefd047d72f8a
Usuario <Usuario@danemaker>
2017-05-08 11:12:15 +0200

    He comentado

---
+++ IG/src/IG/prueba.java
@@ -0,0 +1,14 @@
+package IG;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Usuario
+ */
+public class prueba {
+    //Hola peaso kvron;
+}
